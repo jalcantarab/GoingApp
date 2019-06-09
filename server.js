195631356e151bb98d28618efc41f1de5c0b4828
jalcantarab <jorgeakairos@gmail.com>
2019-06-09 16:28:43 -0700

    First Production release
    Changed to HTTP port. Included all necessary packages, linked to Heroku hosting

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000,
+  port = process.env.PORT || 8080,
   mongoose = require('mongoose'),
   Task = require('./api/models/todoTaskModel'), //created model loading here
   Profile = require('./api/models/profile'),
